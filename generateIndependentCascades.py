ca836b11bb6edc54670c3bd9d935b0e79ea362cd
Jonas Gebele <jonasgebele@gmail.com>
2024-09-09 14:03:49 +0200

    added tqdm status progress bar in command line to see progress

--- generateIndependentCascades.py
+++ generateIndependentCascades.py
@@ -4,7 +4,7 @@ import numpy as np
 import concurrent.futures
 import pickle
 import time
-
+from tqdm import tqdm
 
 def rnd_data_point(maxinc, maxexc, fprs=None):
     n_included = random.randint(1, maxinc)
@@ -14,31 +14,23 @@ def rnd_data_point(maxinc, maxexc, fprs=None):
     cascade = cascadeUtils.create_padded_cascade(
         revoked, valid, maxinc, maxexc, fprs
     )
-    return cascadeUtils.vectorize_cascade(cascade),[n_included,n_excluded]
-
+    return cascadeUtils.vectorize_cascade(cascade), [n_included, n_excluded]
 
 def generate_data(maxinc, maxexc, n_samples=100_000, fprs=None):
     X = np.empty([n_samples, cascadeUtils.vectorized_cascade_size()])
     y = np.empty([n_samples, 2])
     with concurrent.futures.ProcessPoolExecutor(max_workers=6) as executor:
-        future_to_data = {
-            executor.submit(rnd_data_point, maxinc, maxexc, fprs): i
-            for i in range(n_samples)
-        }
-        for future in concurrent.futures.as_completed(future_to_data):
-            i = future_to_data[future]
+        futures = [executor.submit(rnd_data_point, maxinc, maxexc, fprs) for _ in range(n_samples)]
+        
+        for i, future in enumerate(tqdm(concurrent.futures.as_completed(futures), total=n_samples, desc="Generating data")):
             try:
                 data = future.result()
             except Exception as exc:
                 print("%r generated an exception: %s" % (i, exc))
             else:
-                if i % 100 == 0:
-                    print(f"Data point {i}")
-                    print(data[0])
                 X[i, :] = data[0]
                 y[i, :] = data[1]
     return [X, y]
-
 
 if __name__ == "__main__":
     print("Generating data...")
