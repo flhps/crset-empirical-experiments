02cf7d64e4a42836860dc3c7f5ae48478015a865
Jonas Gebele <jonasgebele@gmail.com>
2024-09-26 15:04:08 +0200

    csv file generation

--- generateRawCascadeSeries.py
+++ generateRawCascadeSeries.py
@@ -5,7 +5,7 @@ import numpy as np
 import concurrent.futures
 import pickle
 import time
-
+from tqdm import tqdm
 
 def generate_data_series(
     num_included: int,
@@ -42,7 +42,6 @@ def generate_data_series(
     
     return bitstrings
 
-
 def rnd_data_point(maxinc, maxexc, fprs=None):
     n_included = random.randint(1, maxinc)
     n_excluded = random.randint(1, maxexc)
@@ -50,7 +49,6 @@ def rnd_data_point(maxinc, maxexc, fprs=None):
         n_included,
         n_excluded,
     ]
-
 
 def generate_data(maxinc, maxexc, n_samples=100_000, fprs=None):
     X = []  # This will be a list of lists of bitstrings
@@ -61,27 +59,34 @@ def generate_data(maxinc, maxexc, n_samples=100_000, fprs=None):
             executor.submit(rnd_data_point, maxinc, maxexc, fprs): i
             for i in range(n_samples)
         }
-        for future in concurrent.futures.as_completed(future_to_data):
-            i = future_to_data[future]
-            try:
-                data = future.result()
-            except Exception as exc:
-                print("%r generated an exception: %s" % (i, exc))
-            else:
+        
+        # Create a tqdm progress bar
+        with tqdm(total=n_samples, desc="Generating data points") as pbar:
+            for future in concurrent.futures.as_completed(future_to_data):
+                i = future_to_data[future]
+                try:
+                    data = future.result()
+                except Exception as exc:
+                    print("%r generated an exception: %s" % (i, exc))
+                else:
+                    X.append(data[0])  # Append the list of bitstrings
+                    y[i, :] = data[1]
+                    pbar.update(1)  # Update the progress bar
+                
                 if i % 100 == 0:
-                    print(f"Data point {i}")
-                    print(f"Number of filters: {len(data[0])}")
-                X.append(data[0])  # Append the list of bitstrings
-                y[i, :] = data[1]
+                    pbar.set_postfix({"Filters": len(data[0])})
     
     return [X, y]
 
-
 if __name__ == "__main__":
     print("Generating data...")
-    d = generate_data(1000, 10000, 10000)
-    print("Done generating data.")
+    start_time = time.time()
+    d = generate_data(10000, 100000, 100000)
+    end_time = time.time()
+    print(f"Done generating data. Time taken: {end_time - start_time:.2f} seconds")
+    
     fname = f"data/training-data-bitstrings-{time.time_ns()}.pkl"
+    print(f"Saving data to {fname}...")
     with open(fname, "wb") as outp:
         pickle.dump(d, outp, pickle.HIGHEST_PROTOCOL)
-    print("done")
+    print("Data saved successfully.")
